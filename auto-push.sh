4d48da5dfdbacd07ff848fe717dea6bb23973a87
KaLYes <klyanevv@gmail.com>
2025-05-08 17:52:39 +0300

    Fixes

--- auto-push.sh
+++ auto-push.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-
-#Error handling
+# Error handling: exit on error and log the error
 set -eE
 exec 2> >(while IFS= read -r line; do echo "$(date '+%m-%d %H:%M') - $line"; done >> output.log)
+trap 'echo "Problem: check output.log file!" >&2; exit 1' ERR
 
-trap 'echo "Problem: check output.log file!"; exit 1' ERR
-
-#Path of the directory we want to push to GitHub
+# Navigate to the project directory
 cd /home/kalyes/github
 
-#Pushing the changes
+# Stage all changes
 git add .
-read -p  "Commit message: " comm_msg
 
-#Checking if there is any changes
+#Check git status
+git status
+
+# Check if anything is actually staged for commit
 if git diff --cached --quiet; then
   echo "Nothing staged to commit."
   exit 0
 fi
 
+# Ask for commit message
+read -p "Enter commit message: " comm_msg < /dev/tty
+
+# Commit the changes
 git commit -m "$comm_msg"
-read -p "Do you want to continue and push the commit? (y/n):" choice
-if [[ "$choice" == "y" ]];then
+
+# Confirm pushing the commit
+read -p "Do you want to continue and push the commit? (y/n): " choice
+if [[ "$choice" == "y" ]]; then
   git push -u origin main
   echo "Changes pushed to Github"
 else
